288be323eb783be9439fe63092684dbfb6c0d759
Logan Hennessy <loganjhennessy@gmail.com>
2017-10-17 16:54:30 -0700

    Add documentation to zipcoderequest

--- rentscraper/zipcoderequest.py
+++ rentscraper/zipcoderequest.py
@@ -1,11 +1,25 @@
-'''rentscraper.zipcoderequest
-'''
+"""rentscraper.zipcoderequest"""
 import os
 import requests
 
 class ZipCodeRequest(object):
+    """Allows for encapsulated configuration and use of Zip Code API.
+
+    Attributes:
+        apikey: str containing Zip Code API key
+        base: str containing base URL of Zip Code ZPI
+        city: str of city to search for zips
+        form: str indicating format of return from API
+        state: str of state to search for zips
+    """
 
     def __init__(self, city, state):
+        """Inits ZipCodeRequest with city and state.
+
+        apikey is set to value of ZIP_KEY environment variable
+        base is hardcoded to Zip Code API URL
+        form is hardcoded to 'json'
+        """
         self.apikey = os.environ['ZIP_KEY']
         self.base = (
             'https://www.zipcodeapi.com/rest/{}/city-zips.{}/{}/{}'
@@ -15,6 +29,14 @@ class ZipCodeRequest(object):
         self.state = state
 
     def execute(self):
+        """Uses attributes to make a Zip Code API query.
+
+        Formats base (url) attribute with all required parameters to make a
+        valid request.
+
+        Returns
+            zipcodes: a list of zipcodes returned fro the API
+        """
         url = self.base.format(
             self.apikey,
             self.form,
