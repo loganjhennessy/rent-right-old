69375ea99474b366affb91e650e6cf8abdfc0663
Logan Hennessy <loganjhennessy@gmail.com>
2017-10-17 16:46:19 -0700

    Break up code into functions and add documentation.

--- rentscraper/scraper.py
+++ rentscraper/scraper.py
@@ -28,19 +28,59 @@ def main(argv):
     city = argv[1]
     state = argv[2]
 
+    zipcodes = get_zips(city, state)
+
     mongoclient = MongoClient('localhost', 27017)
 
-    zipcoderequest = ZipCodeRequest(city, state, mongoclient)
+    run_search(city, zipcode, mongoclient)
+    scrape_content(city, zipcode, mongoclient)
+
+def get_zips(city, state):
+    """Get zip codes for the input city and state
+
+    Uses the Zip Code API at https://www.zipcodeapi.com. Requies ZIP_KEY
+    environment variable set to your Zip Code API key. This can be obtained for
+    free by registering on their website.
+
+    Arguments:
+        city: str, the city to search
+        state: state, the state to search
+
+    Returns:
+        zipcodes: list of zipcodes for the input city and state
+    """
+    zipcoderequest = ZipCodeRequest(city, state)
     zipcodes = zipcoderequest.execute()
+    return zipcodes
+
+def run_search(city, zipcode, mongoclient):
+    """Run a search for listings in a specific zip code and write to mongoDB
+
+    City is required as well because it is part of the base URL for searching.
+
+    Arguments:
+        city: str, the city to use in the URL
+        zipcode: str, the zipcode to search in for listings
+        mongoclient: MongoClient object in which to write results
+    """
+    logger = get_configured_logger('DEBUG', __name__)
     for zipcode in zipcodes:
         zipcodesearch = ZipCodeSearch(city, zipcode, mongoclient)
         zipcodesearch.execute()
         logger.info('Compiled results for zip code {}'.format(zipcode))
 
+def scrape_content(zipcode, mongoclient):
+    """Scrape listing content for a particular zip code and write to mongo
+
+    Arguments:
+        zipcode: str, the zipcode to search in for listings
+        mongoclient: MongoClient object in which to write results
+    """
+    logger = get_configured_logger('DEBUG', __name__)
     for zipcode in zipcodes:
-        contentscraper = ContentScraper(city, zipcode, mongoclient)
+        contentscraper = ContentScraper(zipcode, mongoclient)
         contentscraper.execute()
-        logger.info('Gathered listings for zip code {}'.format(zipcode)
+        logger.info('Gathered listings for zip code {}'.format(zipcode))
 
 if __name__ == '__main__':
     main(sys.argv)
