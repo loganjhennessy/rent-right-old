994f32ac4e4d728fa736f659d5e77da3ecd7bca6
Logan Hennessy <loganjhennessy@gmail.com>
2017-10-19 10:31:13 -0700

    Implement Listing class and adjust batch clean to use it appropriately.

--- rentscraper/batchclean.py
+++ rentscraper/batchclean.py
@@ -15,15 +15,18 @@ def cleanlistings(listings):
         listings: pymongo cursor containing all listings
 
     Returns:
+        attrs: set of boolean attributes found in all the listings
         units: list of Unit objects representing the cleaned listings
     """
     units = []
+    attrs = set()
     for listing in listings:
         l = Listing(listing['content'])
         unit = l.clean()
         units.append(unit)
+        attrs.update(l.getattrs())
 
-    return units
+    return attrs, units
 
 def queryforlistings(mongoclient):
     """Get all listings in the mongo database
@@ -39,15 +42,26 @@ def queryforlistings(mongoclient):
     listings = listing_collection.find(query)
     return listings
 
-def writetomongo(mongoclient):
-    """Write unit objects to mongo database
+def writeattrstomongo(mongoclient, attrs):
+    """Write a set of attributes to mongo database.
+
+    Arguments:
+        attrs: a set of attributes to write to mongo
+    """
+    attr_collection = mongoclient.scraper.attr
+    for attr in attrs:
+        if not attr_collection.find_one({"attr": attr})
+            attr_collection.insert_one({"attr": attr})
+
+def writeunitstomongo(mongoclient, units):
+    """Write unit objects to mongo database.
 
     Arguments:
         mongoclient: mongo db to write to
     """
     unit_collection = mongoclient.scraper.unit
     listing_collection = mongoclient.scraper.listing
-    for unit in unit_collection
+    for unit in units
         unit_collection.insert_one(dict(unit))
         query = {"_id": unit['listing_id']}
         update = {"$set": {"content_parsed": True}}
@@ -59,8 +73,9 @@ def main(argv):
 
     mongoclient = MongoClient('localhost', 27017)
     listings = queryforlistings(mongoclient)
-    units = cleanlistings(listings)
-    writetomongo(units)
+    attrs, units = cleanlistings(listings)
+    writeunitstomongo(mongoclient, units)
+    writeattrstomongo(mongoclient, attrs)
 
 if __name__ == '__main__':
     app.run(main)
--- rentscraper/listing.py
+++ rentscraper/listing.py
@@ -4,26 +4,104 @@ from bs4 import BeautifulSoup
 from unit import Unit
 
 class Listing(object):
+    """Implements a Listing.
+
+    A listing contains the raw HTML contents of a listing and the associated
+    methods required to clean the contents and get usable features.
+
+    Attributes:
+        attrset: set of attributes encountered in this listing
+        content: str of raw HTML content of the listing
+        soup: BeautifulSoup object of content parsed using 'html.parser'
+        unit: Unit object for content
+    """
 
     def __init__(self, content):
+        """Inits a Listing with the raw HTML content."""
+        self.attrset = set()
         self.content = content
         self.soup = BeautifulSoup(self.content)
         self.unit = Unit()
 
     def clean(self):
+        """Executes the cleaning of a listing."""
         self._attributes()
+        self._description()
         self._imagemeta()
         self._price()
-        self._sizeinfo()
+        self._title()
+
+    def getattrs(self):
+        """Returns the set of attributes encountered in this listing.
+
+        Returns:
+            attrset: set of attributes encountered in this listing
+        """
+        return self.attrset
 
     def _attributes(self):
+        """Parses all attributes with the 'attrgroup' class.
+
+        This includes both the housing info (beds, baths, sqft) and the boolean
+        attributes describing the unit (W/D in unit, etc).
+        """
+        attrgrouptags = self.soup.select('.attrgroup')
+        housinginfo = attrgrouptags[0]
+        self._parsehousinginfo(housinginfo)
+        attrinfo = attrgrouptags[1]
+        self._parseattrinfo(attrinfo)
+
+    def _description(self):
+        """Parses the text contained in the listing description."""
         pass
 
     def _imagemeta(self):
-        pass
+        """Parses the meta data of the images."""
+        imageinfostr = self.soup.select('.slider-info')[0].text
+        num_images = imageinfo.split()[-1]
+        self.unit['num_images'] = int(num_images)
+
+    def _parseattrinfo(self, attrinfo):
+        """Parses the boolean attributes about the listing.
+
+        Sets a boolean attribute to true in self.unit for each attribute that is
+        found in the listing.
+
+        Arguments:
+            attrinfo: BeautifulSoup tag containing attrs.
+        """
+        attributes = attrgroup.findAll('span')
+        for attr in attributes:
+            self.attrset.add(attr.text)
+            self.unit[attr.text] = True
+
+    def _parsehousinginfo(self, housinginfo):
+        """Parses the housing attributes in the listing.
+
+        Sets 'bedrooms' and 'bathrooms' attributes of self.unit.
+
+        Arguments:
+            housinginfo: BeautifulSoup tag with housing info.
+        """
+        sharedlinebubbles = housinginfo.select('.shared-line-bubble')
+        rooms = sharedlinebubbles[0].text
+        bedrooms, bathrooms = rooms.split(' / ')
+        self.unit['bedrooms'] = bedrooms.strip('BR')
+        self.unit['bathrooms'] = bedrooms.strip('Ba')
 
     def _price(self):
-        self.unit['price'] = self.soup.select('.price')[0].text
+        """Parses the price in the listing.
 
-    def _sizeinfo(self):
-        pass
+        Sets 'price' attribute of self.unit.
+        """
+        pricetag = self.soup.find('span', {'class': 'price'})
+        pricestr = pricetag.text.strip('$')
+        self.unit['price'] float(pricestr)
+
+    def _title(self):
+        """Parse the title of the listsing.
+
+        Sets 'title' attribute of self.unit.
+        """
+        titletext = self.soup.find('span', {'id': 'titletextonly'}).text
+        self.unit['title'] = titletext
