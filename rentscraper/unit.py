f2abed69e8d50cd17d079aa4e9956ed733759d39
Logan Hennessy <loganjhennessy@gmail.com>
2017-10-18 17:37:26 -0700

    Create files for listing and unit classes, used by batchclean.)

---
+++ rentscraper/unit.py
@@ -0,0 +1,23 @@
+"""rentscraper.unit"""
+
+class Unit(object):
+    """
+
+    Attributes:
+        apartment: bool
+        bedrooms: int
+        bathrooms: int
+        cats_ok: bool
+        dogs_ok: bool
+        laundry_on_site: bool
+        no_smoking: bool
+        num_images: int
+        off_street_parking: bool
+        price: float
+        street_parking: bool
+        sqft: int
+        wheelchair_accessible: bool
+    """
+
+    def __init__(self):
+        pass
