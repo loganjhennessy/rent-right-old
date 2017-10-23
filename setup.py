518b50ab65f488114840531580a10688ce90a945
Logan Hennessy <loganjhennessy@gmail.com>
2017-10-23 16:36:09 -0700

    Create setup script for the project

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='rentright', version='1.0', author='lhennessy',
+    author_email='loganjhennessy@gmail.com',
+    packages=['bin', 'model', 'scraper', 'scrubber', 'utils'],
+    url='https://github.com/loganjhennessy/rent-right'
+)
