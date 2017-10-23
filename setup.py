504c884546a78d0adf5deaeff928b25dab26fe69
Logan Hennessy <loganjhennessy@gmail.com>
2017-10-23 16:38:30 -0700

    Rectify package definitions.

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from distutils.core import setup
 setup(
     name='rentright', version='1.0', author='lhennessy',
     author_email='loganjhennessy@gmail.com',
-    packages=['bin', 'model', 'scraper', 'scrubber', 'utils'],
+    packages=[
+        'rentright',
+        'rentright.bin', 
+        'rentright.model', 
+        'rentright.scraper', 
+        'rentright.scrubber', 
+        'rentright.utils',
+    ],
     url='https://github.com/loganjhennessy/rent-right'
 )
