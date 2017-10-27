d67326e7dc564bfe85053bbdad9dee989cdc6e72
Logan Hennessy <loganjhennessy@gmail.com>
2017-10-27 07:08:18 -0700

    Implement Preprocessor class.

--- rentright/model/preprocessor.py
+++ rentright/model/preprocessor.py
@@ -3,19 +3,26 @@ import pandas as pd
 
 from rentright.utils.mongo import get_mongoclient
 
+
 class Preprocessor(object):
     """Preprocesses data for training the rentright model."""
-    def __init__(self, mongoclient):
+    def __init__(self,
+                 mongoclient,
+                 maxprice=25000,
+                 maxsqft=10000,
+                 zipcode=None):
         self.df = None # set by process() method
         self.mongoclient = mongoclient
-        self.maxprice
-        self.maxsqft
+        self.maxprice = maxprice
+        self.maxsqft = maxsqft
         self.units = self.mongoclient.scraper.unit.find()
+        self.zipcode = zipcode
 
-    def getfeatures(self, features=None):
-        if self.df == None and features == None:
-            self.process()
-            features = list(self.df.columns).remove('_id')
+    def getfeatures(self):
+        if self.df == None:
+            excludelist = ['_id', 'description', 'listing_id', 'price', 'title']
+            features = list(set(self.df.columns) - set(excludelist))
+            self.makedataframe()
 
         X = self.df[features]
         return X
@@ -24,7 +31,7 @@ class Preprocessor(object):
         y = self.df['price']
         return y
 
-    def process(self):
+    def makedataframe(self):
         df = pd.DataFrame(list(self.units))
         df.fillna(False, inplace=True)
         df = df[
@@ -32,10 +39,15 @@ class Preprocessor(object):
             (df['sqft'] != 0) &
             (df['sqft'] < self.maxsqft)
         ]
+
+        if self.zipcode:
+            df = df[df['zipcode'] == self.zipcode]
+
         self.df = df
         return df
+
 
 if __name__ == '__main__':
     mongoclient = get_mongoclient()
     preprocessor = Preprocessor(mongoclient)
-    preprocessor.process()
+    preprocessor.makedataframe()
