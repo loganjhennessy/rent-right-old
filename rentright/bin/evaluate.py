1562f1367de594f0825a0417878da344da0d4ddf
Logan Hennessy <loganjhennessy@gmail.com>
2017-10-27 07:07:45 -0700

    Build an evaluation script to run a model an print the results.

--- rentright/bin/evaluate.py
+++ rentright/bin/evaluate.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score
 
+from rentright.model.evaluator import Evaluator
 from rentright.model.preprocessor import Preprocessor
 from rentright.utils.mongo import get_mongoclient
 
@@ -14,13 +15,17 @@ def score(model, X, y):
         )
     )
     r2 = list(cross_val_score(model, X, y))
+    printtable(error, r2)
 
-    print('Scores: %s' % error)
-    print('Mean: %s' % np.mean(error))
-    print('Std:  %s' % np.std(error))
-    print('R^2: %s' % r2)
-    print('Mean: %s' % np.mean(r2))
-    print('Std: %s' % np.std(r2))
+def printtable(error, r2):
+    headers = ['Run', 'Error', 'R<sup>2</sup>']
+    header = '|' + '|'.join(headers) + '|'
+    align = '|:-:|--:|--:|'
+    rows = ['|{}|{:6.2f}|{:6.2f}|'.format(i + 1, error, r2) for i, (error, r2) in enumerate(zip(error, r2))]
+    rows = '\n'.join(rows)
+    mean = '|Mean|{:6.2f}|{:6.2f}|'.format(np.mean(error), np.mean(r2))
+    std = '|Std|{:6.2f}|{:6.2f}|'.format(np.std(error), np.std(r2))
+    return '\n'.join([header, align, rows, mean, std])
 
 def main():
     mongoclient = get_mongoclient()
@@ -30,7 +35,10 @@ def main():
     y = preprocessor.getlabels()
 
     model = LinearRegression()
-    score(model, X, y)
+    evaluator = Evaluator(model, X, y)
+    evaluator.evaluate()
+    print('Ran on {} units...'.format(X.shape[0]))
+    print(evaluator.results())
 
 if __name__ == '__main__':
     main()
