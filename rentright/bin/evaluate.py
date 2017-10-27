b5a00f57b50e2289e0b0fd78c56ca2916579da16
Logan Hennessy <loganjhennessy@gmail.com>
2017-10-27 07:42:00 -0700

    Add import for LinearRegression

--- rentright/bin/evaluate.py
+++ rentright/bin/evaluate.py
@@ -1,31 +1,8 @@
-import numpy as np
-
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import cross_val_score
 
 from rentright.model.evaluator import Evaluator
 from rentright.model.preprocessor import Preprocessor
 from rentright.utils.mongo import get_mongoclient
-
-def score(model, X, y):
-    error = list(map(
-        abs,
-        cross_val_score(
-            model, X, y, cv=5, scoring='neg_mean_absolute_error')
-        )
-    )
-    r2 = list(cross_val_score(model, X, y))
-    printtable(error, r2)
-
-def printtable(error, r2):
-    headers = ['Run', 'Error', 'R<sup>2</sup>']
-    header = '|' + '|'.join(headers) + '|'
-    align = '|:-:|--:|--:|'
-    rows = ['|{}|{:6.2f}|{:6.2f}|'.format(i + 1, error, r2) for i, (error, r2) in enumerate(zip(error, r2))]
-    rows = '\n'.join(rows)
-    mean = '|Mean|{:6.2f}|{:6.2f}|'.format(np.mean(error), np.mean(r2))
-    std = '|Std|{:6.2f}|{:6.2f}|'.format(np.std(error), np.std(r2))
-    return '\n'.join([header, align, rows, mean, std])
 
 def main():
     mongoclient = get_mongoclient()
