33db74b01b3bebaf4a9de81d5f15253198a6a118
Logan Hennessy <loganjhennessy@gmail.com>
2017-11-01 04:01:19 +0000

    Update flaskapp

--- rentright/flaskapp/flaskapp.py
+++ rentright/flaskapp/flaskapp.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import pickle
 
-from flask import Flask, render_template, request, json
-
 from rentright.scrape.contentscraper import ContentScraper
 from rentright.scrub.listing import Listing
+
+from flask import Flask, render_template, request, json
+from sklearn.ensemble import RandomForestRegressor
+
 
 app = Flask(__name__)
 
@@ -29,6 +31,8 @@ def estimate():
     unit = listing.clean()
     actual_price = unit['price']
 
+    print('actual_price: %s' % actual_price)
+
     attr_set = {'house', 'attached garage', 'duplex', 'cottage/cabin',
                 'cats are OK - purrr', 'flat', 'street parking',
                 'laundry on site', 'apartment', 'dogs are OK - wooof',
@@ -43,6 +47,8 @@ def estimate():
                    'bedrooms', 'apartment', 'dogs are OK - wooof',
                    'no smoking', 'bathrooms'} | attr_set
 
+    print('Set the sets')
+
     unit_features = {
         key: val for key, val in unit.data.items() if key in feature_set
     }
@@ -51,14 +57,23 @@ def estimate():
     df = pd.concat([unit_df, empty_df])
     df.fillna(False, inplace=True)
 
+    print('Build the df')
+
     features = list(
         set(df.columns) - {'_id', 'description', 'listing_id', 'price', 'title'}
     )
     X = df[features]
 
+    print('Got some features')
+
     # Feed all the attributes into the model to make one estimate
     #   Set estimate = 'estimate'
+    rentrightmodel = pickle.load(open('/home/ubuntu/rent-right/rentright/data/rentrightmodel-v1.pkl', 'rb'))
     estimated_price = rentrightmodel.predict(X)
+
+    print('estimated_price: %s' % estimated_price)
+
+    model_pkl.close()
 
     # Return the estimate and actual price, which gets set on the client side
     return json.dumps({
@@ -68,5 +83,4 @@ def estimate():
     })
 
 if __name__ == '__main__':
-    rentrightmodel = pickle.load(open('../data/rentrightmodel.pkl', 'rb'))
     app.run()
