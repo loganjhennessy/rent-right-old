247dc8770bb48552ef41ac8aacf81907676e227f
Logan Hennessy <loganjhennessy@gmail.com>
2017-11-05 00:56:51 +0000

    Remove the file handler from logger.

--- rentright/utils/log.py
+++ rentright/utils/log.py
@@ -6,10 +6,6 @@ loggers = {}
 
 def get_configured_logger(level, name):
 
-    home_dir = os.environ['HOME']
-    dir_path = home_dir + '/rent-right'
-    logfile = dir_path + '/logs/' + name + '.log'
-
     if loggers.get(name):
         return loggers.get(name)
 
@@ -21,17 +17,12 @@ def get_configured_logger(level, name):
             ch = logging.StreamHandler()
             ch.setLevel(getattr(logging,level))
 
-            fh = logging.FileHandler(logfile)
-            fh.setLevel(getattr(logging,level))
-
             format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
             formatter = logging.Formatter(format_string)
 
             ch.setFormatter(formatter)
-            fh.setFormatter(formatter)
 
             logger.addHandler(ch)
-            logger.addHandler(fh)
 
         loggers.update(dict(name=logger))
 
